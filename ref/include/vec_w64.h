103c3a30a55c2ec4ca7a07934124c0f42d5c7cb6
Lilith-Schreiber <72674487+Lilith-Schreiber@users.noreply.github.com>
2024-11-21 15:26:16 +0800

    modify to AVX2 instructions

--- ref/include/vec_w64.h
+++ ref/include/vec_w64.h
@@ -8,35 +8,45 @@
 
 #define aligned __attribute__((aligned(64)))
 
-#define ADD_w64(a, b) _mm512_add_epi64(a, b)
-#define ADD_M_w64(a, b, m) _mm512_mask_add_epi64(a, m, a, b)
-#define SUB_w64(a, b) _mm512_sub_epi64(a, b)
-#define SUB_M_w64(a, b, m) _mm512_mask_sub_epi64(a, m, a, b)
+#define pmod_mat_w64_t __m256i
+#define pmod_mat_mask_w64_t __mmask8
 
-#define MULLO_w64(a, b) _mm512_mullo_epi64(a, b)
+#define ADD_w64(a, b) _mm256_add_epi64(a, b)
+// #define ADD_M_w64(a, b, m) _mm_mask_add_epi64(a, m, a, b) // need to modify
+#define SUB_w64(a, b) _mm256_sub_epi64(a, b)
+// #define SUB_M_w64(a, b, m) _mm_mask_sub_epi64(a, m, a, b) // need to modify
 
-#define SRLI_w64(a, b) _mm512_srli_epi64(a, b)
-#define SLLI_w64(a, b) _mm512_slli_epi64(a, b)
+pmod_mat_w64_t ADD_M_w64(pmod_mat_w64_t a, pmod_mat_w64_t b, pmod_mat_mask_w64_t m);
+pmod_mat_w64_t SUB_M_w64(pmod_mat_w64_t a, pmod_mat_w64_t b, pmod_mat_mask_w64_t m);
 
-#define AND_w64(a, b) _mm512_and_epi64(a, b)
-#define OR_w64(a, b) _mm512_or_epi64(a, b)
-#define XOR_w64(a, b) _mm512_xor_epi64(a, b)
+#define MULLO_w64(a, b) _mm256_mullo_epi32(a, b) // no 64, and 32 and 64 are almost the same
 
-#define GT_w64(a, b) _mm512_cmpgt_epi64_mask(a, b)
-#define GE_w64(a, b) _mm512_cmpge_epi64_mask(a, b)
-#define LT_w64(a, b) _mm512_cmplt_epi64_mask(a, b)
-#define LE_w64(a, b) _mm512_cmple_epi64_mask(a, b)
-#define EQ_w64(a, b) _mm512_cmpeq_epi64_mask(a, b)
-#define NEQ_w64(a, b) _mm512_cmpneq_epi64_mask(a, b)
+#define SRLI_w64(a, b) _mm256_srli_epi64(a, b)
+#define SLLI_w64(a, b) _mm256_slli_epi64(a, b)
+
+#define AND_w64(a, b) _mm256_and_si256(a, b)
+#define OR_w64(a, b) _mm256_or_si256(a, b)
+#define XOR_w64(a, b) _mm256_xor_si256(a, b)
+
+// #define GT_w64(a, b) _mm256_cmpgt_epi64_mask(a, b)
+// #define GE_w64(a, b) _mm256_cmpge_epi64_mask(a, b)
+// #define LT_w64(a, b) _mm256_cmplt_epi64_mask(a, b)
+// #define LE_w64(a, b) _mm256_cmple_epi64_mask(a, b)
+// #define EQ_w64(a, b) _mm256_cmpeq_epi64_mask(a, b)
+// #define NEQ_w64(a, b) _mm256_cmpneq_epi64_mask(a, b)
+
+__mmask8 GT_w64(__m256i a, __m256i b);
+__mmask8 GE_w64(__m256i a, __m256i b);
+__mmask8 LT_w64(__m256i a, __m256i b);
+__mmask8 LE_w64(__m256i a, __m256i b);
+__mmask8 EQ_w64(__m256i a, __m256i b);
+__mmask8 NEQ_w64(__m256i a, __m256i b);
 
 #define SET1_CT_w64(a) {a, a, a, a, a, a, a, a}
-#define SET1_w64(a) _mm512_set1_epi64(a)
+#define SET1_w64(a) _mm256_set1_epi64x(a)
 
-#define LOAD_w64(a) _mm512_load_epi64(a)
-#define STORE_w64(a, b) _mm512_store_epi64(a, b)
-
-#define pmod_mat_w64_t __m512i
-#define pmod_mat_mask_w64_t __mmask8
+#define LOAD_w64(a) _mm256_loadu_si256(a) // if there is something wrong with this function, add `&` for the parameter input
+#define STORE_w64(a, b) _mm256_store_si256(a, b)
 
 uint64_t extract_vec_w64(pmod_mat_w64_t x, int pos);
 uint32_t extract_mask_w64(pmod_mat_mask_w64_t x, int pos);
